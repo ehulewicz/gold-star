faeb0c9dfe71af7070d13247a04a28d9e3a8e75a
ehulewicz <eahtree@gmail.com>
2024-10-10 06:27:56 +0200

    moved to wsl directory

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Gold Star Home Please</h1>
+      <p1>Your truster partner for quality home improvments</p1>
     </div>
   );
 }
